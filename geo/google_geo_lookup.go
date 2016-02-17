60dbc92ceab575f9280e883fd5365e13ddc21533
Scott Kidder <kidder.scott@gmail.com>
2016-02-16 18:03:53 -0800

    Use a local cache of timezone info for sensor locations

--- geo/google_geo_lookup.go
+++ geo/google_geo_lookup.go
@@ -1,15 +1,18 @@
 package geo
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/patrickmn/go-cache"
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
 )
 
 type GoogleGeoLookup struct {
-	googleApiKey string
-	mapsClient   *maps.Client
+	googleApiKey  string
+	mapsClient    *maps.Client
+	locationCache *cache.Cache
 }
 
 type TimezoneInfo struct {
@@ -18,7 +21,10 @@ type TimezoneInfo struct {
 }
 
 func NewGoogleGeoLookup(googleApiKey string) *GoogleGeoLookup {
-	return &GoogleGeoLookup{googleApiKey: googleApiKey}
+	return &GoogleGeoLookup{
+		googleApiKey:  googleApiKey,
+		locationCache: cache.New(1*time.Hour, 1*time.Minute),
+	}
 }
 
 func (g *GoogleGeoLookup) Init() error {
@@ -28,6 +34,13 @@ func (g *GoogleGeoLookup) Init() error {
 }
 
 func (g *GoogleGeoLookup) FindTimezoneForLocation(latitude, longitude float64) (*TimezoneInfo, error) {
+	// check local cache for timezone info for this location
+	locationHash := fmt.Sprintf("%f,%f", latitude, longitude)
+	if x, found := g.locationCache.Get(locationHash); found {
+		return x.(*TimezoneInfo), nil
+	}
+
+	// cache-miss, request the timezone info from Google Maps API
 	r := &maps.TimezoneRequest{
 		Location: &maps.LatLng{
 			Lat: latitude,
@@ -38,7 +51,10 @@ func (g *GoogleGeoLookup) FindTimezoneForLocation(latitude, longitude float64) (
 
 	resp, e := g.mapsClient.Timezone(context.Background(), r)
 	if e == nil {
-		return &TimezoneInfo{TimeZoneID: resp.TimeZoneID, TimeZoneName: resp.TimeZoneName}, nil
+		// store results in local cache
+		locationInfo := &TimezoneInfo{TimeZoneID: resp.TimeZoneID, TimeZoneName: resp.TimeZoneName}
+		g.locationCache.Set(locationHash, locationInfo, cache.DefaultExpiration)
+		return locationInfo, nil
 	}
 	return nil, e
 }
